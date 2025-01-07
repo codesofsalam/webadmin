ce6c98a20d0ed8416c9416d8fdb3df75e721f87c
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-01-08 01:37:53 +0500

    commit

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send("Router");
-});
+
+router.route("/").get((req, res) => {
+    res.status(200).send("Router Route");
+  });
+
+
+router.route("/register").get((req,res)=>{
+    res.status(200).send("Register Route")
+})
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,15 +4,6 @@ const router = require("./router/auth-router");
 
 app.use("/api/auth", router);
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello Sam!");
-});
-
-app.get("/register", (req, res) => {
-  res.status(200).send("Welcome to Registration Page");
-});
-
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
