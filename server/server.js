05aa39fc77ccde9c3ff7846c008b5611b08c3bf2
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-01-07 22:28:08 +0500

    commit

--- server/server.js
+++ server/server.js
@@ -5,9 +5,13 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello Sam!");
 });
 
-app.get("/register", (req,res)=>{
-    res.status(200).send("Welcome to Registration Page")
-})
+app.get("/register", (req, res) => {
+  res.status(200).send("Welcome to Registration Page");
+});
+
+app.get("/login", (req, res) => {
+  res.status(200).send("Welcome to Login Page");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
